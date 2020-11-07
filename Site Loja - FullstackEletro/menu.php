9b482d5e0477b0224e00eba1fe3f938871226c75
Roger <72757812+williroger@users.noreply.github.com>
2020-11-06 23:56:42 -0300

    Update menu.php

--- Site Loja - FullstackEletro/menu.php
+++ Site Loja - FullstackEletro/menu.php
@@ -1,9 +1,8 @@
 <header>
- <nav class="menu">
-            <a href="index.php"><img src="./imagens/logo.png" width="100px">
-            <a href="produtos.php">| Nosso Produtos |</a>
-            <a href="lojas.php">Nossas Lojas |</a>
-            <a href="contato.php">Fale Conosco  |</a>
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="index.php"><img src="./imagens/logo.png" width="100px"></a>
+    <a href="produtos.php">Nosso Produtos</a>
+    <a href="lojas.php">Nossas Lojas</a>
+    <a href="contato.php">Fale Conosco</a>
 </nav>
 </header>
-  
