1fb6ace9aa1e4c923b2b324a4890f5b36ae4cc29
Roger <72757812+williroger@users.noreply.github.com>
2020-11-07 00:01:11 -0300

    Update rodape.php

--- Site Loja - FullstackEletro/rodape.php
+++ Site Loja - FullstackEletro/rodape.php
@@ -1,5 +1,16 @@
-<footer id="rodape">
-            <p id="formas_pagamento"><b>Formas de Pagamento:</b></p>
+<footer class="page-footer font-small cyan darken-3">
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-12 py-5">
+      <div class="mb-5 flex-center">
+      <p id="formas_pagamento"><b>Formas de Pagamento:</b></p>
             <img src="./imagens/formas-pg.png" height="42">
-            <p>&copy; Recode Pro</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+<div class="footer-copyright text-center py-3"><p>&copy; Recode Pro</p>
+</div>
 </footer>
